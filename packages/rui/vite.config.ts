056b1de8a4141b66ee19c9fec784d125b7eac013
ConcerroX <concerrox@outlook.com>
2026-07-04 23:47:51 +0800

    Add dependencies;

---
+++ packages/rui/vite.config.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from "node:url"
+
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import dts from "vite-plugin-dts"
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        dts({
+            insertTypesEntry: true,
+            outDirs: ["./dist"],
+            tsconfigPath: "./tsconfig.json",
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    build: {
+        lib: {
+            entry: fileURLToPath(new URL("./src/index.ts", import.meta.url)),
+            formats: ["es"],
+            fileName: "index",
+        },
+        rollupOptions: {
+            external: ["vue"],
+        },
+    },
+})
