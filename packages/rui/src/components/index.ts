f01e7fbeda2d81d6aa9bff6e7e970e4b471a835a
ConcerroX <concerrox@outlook.com>
2026-07-07 15:09:16 +0800

    Fix exports;

--- packages/rui/src/components/index.ts
+++ packages/rui/src/components/index.ts
@@ -1,1 +1,3 @@
-export * from "./textField"
+export { default as RTextField } from "./textField/RTextField.vue"
+export { default as RTextInput } from "./textField/RTextInput.vue"
+export type * from "./textField/types.ts"
